d1ddd939e0456d158301ee0f02b9b58579e117e7
xbid <abdzubaid@gmail.com>
2021-02-11 15:02:42 +0200

    multiply

--- app.js
+++ app.js
@@ -28,11 +28,11 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function multiply(a, b) {
-
+function multiply(a, b) {
+let z = a * b;
   //eslint-disable-line
-
-// }
+  return [z,`The product of ${a} and ${b} is ${z}.`];
+}
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5,9);
